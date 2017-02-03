47adab99c14f91ccc21b3833d5c6a36b96fd1ada
wendingp <wendingp@gmail.com>
2017-02-03 11:36:24 +0800

    Finish lab1

---
+++ cmake-build-debug/CMakeFiles/Berkeley_CS61C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Berkeley_CS61C.dir/proj1/hashtable.c.o"
+  "CMakeFiles/Berkeley_CS61C.dir/proj1/philspel.c.o"
+  "CMakeFiles/Berkeley_CS61C.dir/lab1/helo.c.o"
+  "Berkeley_CS61C.pdb"
+  "Berkeley_CS61C"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Berkeley_CS61C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
