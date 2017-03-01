f56846066e5ea458a52d24a6338e62651bb0263d
wendingp <wendingp@gmail.com>
2017-03-01 18:30:42 +0800

    Fixed quotes bug.

--- DIY/C_comments_deleter.c
+++ DIY/C_comments_deleter.c
@@ -6,8 +6,8 @@
  * Deletes all comments of a C source file.
  * TODO: To handle special case for "\", eg,
  *
- * [code]  //this is a comment \
- *         but this is also a comment [\code]
+ * [code]  //this line is a comment \
+ *         but this line is also a comment [\code]
  */
 void comment_deleter(char *input) {
     FILE *in = fopen(input, "r");
@@ -22,21 +22,20 @@ void comment_deleter(char *input) {
     }
     char c;
     while ((c = fgetc(in)) != EOF) {
-//        printf("%c", c);
         if (c == '\'' || c == '"') {
             /* Quotes, no comment inside. */
             char match = fgetc(in);
             while (1) {
-                fputc(match, out); // Here, match cannot be EOF for a valid C source file.
                 if (match == c) {
                     break;
                 }
+                fputc(match, out); // Here, match cannot be EOF for a valid C source file.
                 match = fgetc(in);
             }
         } else if (c == '/') {
-            /* Maybe a comment, check the next character */
+            /* May be a comment, checks the next character */
             char next_c = fgetc(in);
-            /* Stop checking if end of file. */
+            /* Stops checking if end of file. */
             if (next_c == EOF) {
                 break;
             }
@@ -63,7 +62,7 @@ void comment_deleter(char *input) {
                 continue;
             }
         } else {
-            /* If not two previous cases, just print it to the output. */
+            /* If not comment, just prints it to the output. */
             fputc(c, out);
         }
     }
@@ -71,16 +70,12 @@ void comment_deleter(char *input) {
     fclose(out);
 }
 
-
 int main() {
     char input[256];
     printf("Choose a C source file which you want to delete comments of: ");
     scanf("%s", input);
     printf("Running comment deleter\n");
-
     comment_deleter(input);
-
-    printf("Finish comment deleter.\n");
-
+    printf("Comments deleted.\n");
     return 0;
 }
