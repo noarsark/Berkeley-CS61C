0445150ae301f08c4e449bd9023ecb10fdc92ee8
wendingp <wendingp@gmail.com>
2017-03-10 17:30:49 +0800

    Fixed no quote printed bug.

--- DIY/C_comments_deleter.c
+++ DIY/C_comments_deleter.c
@@ -17,23 +17,21 @@ void comment_deleter(char *input) {
         fprintf(stderr, "File output cannot be opened!\n");
         exit(1);
     }
-    char c;
+    int c;
     while ((c = fgetc(in)) != EOF) {
-        if (c == '\'' || c == '"') {
+        if (c == '\'' || c == '\"') {
             /* String literals, no comment inside. */
             fputc(c, out);
-            char match = fgetc(in);
-            while (1) {
-                if (match == c) {
-                    fputc(match, out);
-                    break;
+            int match;
+            while ((match = fgetc(in)) != c) {
+                fputc(match, out);
+                if (match == '\\') {
+                    fputc(fgetc(in), out);
                 }
-                fputc(match, out);  // Here, match cannot be EOF for a valid C source file.
-                match = fgetc(in);
             }
         } else if (c == '/') {
             /* May be a comment, checks the next character */
-            char next_c = fgetc(in);
+            int next_c = fgetc(in);
             /* Stops checking if end of file. */
             if (next_c == EOF) {
                 break;
@@ -43,7 +41,7 @@ void comment_deleter(char *input) {
                 /* One-line comment detected. */
                 char line[256];
                 do {
-                    if (fgets(line, sizeof line, in) == NULL){
+                    if (fgets(line, sizeof line, in) == NULL) {
                         break;
                     }
                 } while (line[strlen(line) - 2] == '\\');
@@ -53,9 +51,9 @@ void comment_deleter(char *input) {
             if (next_c == '*') {
                 /* Multi-line comment detected */
                 while (1) {
-                    char match_star = getc(in);
+                    int match_star = getc(in);
                     if (match_star == '*') {
-                        char match_back_slash = getc(in);
+                        int match_back_slash = getc(in);
                         if (match_back_slash == '/') {
                             /* Comment closed. */
                             break;
@@ -77,7 +75,7 @@ void comment_deleter(char *input) {
 int main() {
     char input[256];
     printf("Choose the C source file you want to delete comments of: ");
-    scanf("%s", input);
+   scanf("%s", input);
     printf("Running comment deleter...\n");
     comment_deleter(input);
     printf("Comments deleted.\n");
